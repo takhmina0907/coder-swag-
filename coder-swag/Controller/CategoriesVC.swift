fa1a533aa25656934d134a68c0ef0652642d3109
takhmina0907 <lollis0907@gmail.com>
2018-07-21 21:46:10 +0600

    final project

--- coder-swag/Controller/CategoriesVC.swift
+++ coder-swag/Controller/CategoriesVC.swift
@@ -38,6 +38,23 @@ class CategoriesVC: UIViewController, UITableViewDataSource, UITableViewDelegate
             return CategoriesCell()
         }
     }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        let category = DataService.instance.getCategories()[indexPath.row]
+        performSegue(withIdentifier: "ProductsVC", sender: category)
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if let productsVC = segue.destination as? ProductVC{
+            //title in bar
+            let barBtn = UIBarButtonItem()
+            barBtn.title = " "
+            navigationItem.backBarButtonItem = barBtn
+            
+            assert(sender as? Category != nil)
+            productsVC.initProducts(category: sender as! Category)
+        }
+    }
 
 
 }
---
+++ coder-swag/Controller/ProductVC.swift
@@ -0,0 +1,43 @@
+//
+//  ProductVC.swift
+//  coder-swag
+//
+//  Created by Takhmina Talipova on 7/21/18.
+//  Copyright © 2018 Takhmina Talipova. All rights reserved.
+//
+
+import UIKit
+
+class ProductVC: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource {
+    
+    @IBOutlet weak var productsCollection: UICollectionView!
+    
+    private (set) public var products = [Product]()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        productsCollection.dataSource = self
+        productsCollection.delegate = self
+    }
+    func initProducts(category: Category){
+        products = DataService.instance.getProducts(forCategoryTitle: category.title)
+        //for title in bar
+        navigationItem.title = category.title
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return products.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        if let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "ProductCell", for: indexPath) as? ProductCell{
+            let product = products[indexPath.row]
+            cell.updateViews(product: product)
+            return cell
+        }
+        return ProductCell()
+    }
+   
+
+}
