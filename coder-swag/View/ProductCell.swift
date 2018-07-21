20a34995713684129539a7f784f44041bf4b24c1
takhmina0907 <lollis0907@gmail.com>
2018-07-21 19:48:53 +0600

    Collection table view

---
+++ coder-swag/View/ProductCell.swift
@@ -0,0 +1,16 @@
+//
+//  ProductCell.swift
+//  coder-swag
+//
+//  Created by Takhmina Talipova on 7/21/18.
+//  Copyright © 2018 Takhmina Talipova. All rights reserved.
+//
+
+import UIKit
+
+class ProductCell: UICollectionViewCell {
+    @IBOutlet weak var productImage: UIImageView!
+    @IBOutlet weak var productTitle: UILabel!
+    @IBOutlet weak var productPrice: UILabel!
+    
+}
